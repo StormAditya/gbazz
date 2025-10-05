9b01689a85359cdc2fd4e79be3f16bda628e67e9
StormAditya <desaiaditya509@gmail.com>
2025-10-05 11:29:56 +0530

    fixed listed items

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,8 @@ const uploadRoutes = require('./routes/uploadRoutes');
 app.use('/api/items', itemRoutes);
 app.use('/api/upload', uploadRoutes);
 
-
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+const rootDir = path.resolve();
+app.use('/uploads', express.static(path.join(rootDir, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
